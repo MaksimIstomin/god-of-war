289e5659bb3fa083da428ec9d6691a4a92371e10
maks <leon.max.super@gmail.com>
2025-12-05 21:02:35 +0200

    overlay done

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 const burger = document.querySelector('.nav-left-burger')
+const cardBtns = document.querySelectorAll('.editions-card-item-buy')
+const modalClose = document.querySelector('.modal-close')
+const modalPrice = document.querySelector('.modal-form-box-price')
 burger.addEventListener('click',function(){
     burger.nextElementSibling.classList.toggle('show')
+})
+
+cardBtns.forEach(function (element, index) {
+    element.addEventListener('click',function(){
+        modalPrice.textContent = element.previousElementSibling.textContent
+        document.body.firstElementChild.firstElementChild.style.display = 'flex'
+    })
+})
+modalClose.addEventListener('click',()=>{
+    modalClose.parentElement.parentElement.style.display = 'none'
 })
