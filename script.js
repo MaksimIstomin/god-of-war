b4724b9c59e2ac6720973d94092017c4820a0fd9
maks <leon.max.super@gmail.com>
2025-12-07 10:32:35 +0200

    do  more

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+const swiper = new Swiper('.swiper', {
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  slidesPerView:3,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+
+
+
+
+
 const burger = document.querySelector('.nav-left-burger')
 const cardBtns = document.querySelectorAll('.editions-card-item-buy')
 const modalClose = document.querySelector('.modal-close')
